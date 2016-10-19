f7046fc0333d8e61ce6b32e364479a0add0a0c96
Jim Padilla and Margie Nevarez <margie@>
2016-10-19 08:59:13 -0700

    create form that will take input and submit it into an adlib

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("#person1").val();
+    var person2Input = $("#person2").val();
+    var animalInput = $("#animal").val();
+    var exclamationInput = $("#exclamation").val();
+    var verbInput = $("#verb").val();
+    var nounInput = $("#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+    event.preventDefault();
+  });
+});
